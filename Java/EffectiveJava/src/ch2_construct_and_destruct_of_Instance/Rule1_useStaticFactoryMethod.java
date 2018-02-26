1c1516e6641dc0bf462a86c93d0f6032449a22b8
Lenir <1net1@naver.com>
2018-02-26 17:04:21 +0900

    update static factory method examples in circumstances of generic programming, inheritance

--- Java/EffectiveJava/src/ch2_construct_and_destruct_of_Instance/Rule1_useStaticFactoryMethod.java
+++ Java/EffectiveJava/src/ch2_construct_and_destruct_of_Instance/Rule1_useStaticFactoryMethod.java
@@ -1,10 +1,15 @@
 package ch2_construct_and_destruct_of_Instance;
+import java.util.*;
 
 class Spam{
     private int spamNum;
+    Spam(){
+        spamNum = 0;
+    }
     Spam(int _spamNum) {
         spamNum = _spamNum;
     }
+
     public String getMultipleSpams(){
         String result = "";
         for(int i = 0; i < spamNum; i++){
@@ -14,15 +19,29 @@ class Spam{
     }
 }
 
+class SpamChild extends Spam{
+    private int childSpamNum;
+    SpamChild(){
+        childSpamNum = 0;
+    }
+
+    SpamChild(int _childSpamNum){
+        childSpamNum = _childSpamNum;
+    }
+}
+
 class SpamWithFactory{
     private int spamNum;
     SpamWithFactory(int _spamNum) {
         spamNum = _spamNum;
     }
+
+
     public static SpamWithFactory produceSpams(int _spamNum){
         SpamWithFactory spam = new SpamWithFactory(_spamNum);
         return spam;
     }
+
     public String getMultipleSpams(){
         String result = "";
         for(int i = 0; i < spamNum; i++){
@@ -30,15 +49,40 @@ class SpamWithFactory{
         }
         return result;
     }
+
+    public static <K, V> HashMap<K, V> newHashMap(){
+        return new HashMap<K, V>();
+    }
+
 }
+
+//class SpamWithFactoryChild extends SpamWithFactory{
+//
+//}
 
 public class Rule1_useStaticFactoryMethod {
     public static void main(){
+        // **** pros of static factory method ****
+        // ** for readability **
         // default constructor
         Spam spam = new Spam(1);
+
         // static factory method - more readability!
         SpamWithFactory spamWithFactory = SpamWithFactory.produceSpams(5);
         System.out.println(spam.getMultipleSpams());
         System.out.println(spamWithFactory.getMultipleSpams());
+
+        // ** for short code width in generic programming **
+        // default constructor
+        HashMap<Integer, List<String> > map = new HashMap<Integer, List<String> >();
+
+        // static factory method - shorten code width
+        HashMap<Integer, List<String> > map2 = SpamWithFactory.newHashMap();
+
+        // **** cons of static factory method ****
+        // no default constructor - cannot inherit child class
+        // look commented code - SpamWithFactoryChild class
+        Spam child = new SpamChild();
+
     }
 }
